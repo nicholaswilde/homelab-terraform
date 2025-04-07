e18b8653a4cdbc7859f793b0a78681aae6effd4e
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-04-07 13:05:33 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,21 +10,6 @@ terraform {
   }
 }
 
-variable "HOSTNAME" {
-  type      = string
-}
-
-variable "SIZE" {
-  type      = string
-  description = "The desired size for the LXC container's root filesystem (e.g., '8G', '20G')."
-  # Optional: Keep the validation
-  validation {
-    # Regex checks for one or more digits followed by G (for Gigabytes)
-    condition     = can(regex("^\\d+G$", var.SIZE))
-    error_message = "The rootfs size must be a number followed by 'G' (e.g., '8G')."
-  }
-}
-
 provider "proxmox" {  
   # NOTE Optional, but recommended to set to true if you are using self-signed certificates.
   pm_tls_insecure = false
