1c04121ad4976735a6c1d238f9a558d6c7b36ea8
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-04-08 09:17:35 -0700

    Update providers.tf

--- lxc_clone/providers.tf
+++ lxc_clone/providers.tf
@@ -18,7 +18,7 @@ provider "proxmox" {
   pm_api_token_id = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
   pm_log_enable = true
-  pm_log_file   = "terraform-plugin-proxmox.log"
+  pm_log_file   = "tf-plugin-proxmox.log"
   pm_debug      = true
   pm_log_levels = {
     _default    = "debug"
