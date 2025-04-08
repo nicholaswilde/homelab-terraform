b62895e5a5b01f7164d6b1ef7c9cf4e8724e905d
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-04-08 09:28:15 -0700

    Update variables.tf

--- lxc_clone/variables.tf
+++ lxc_clone/variables.tf
@@ -3,17 +3,6 @@ variable "hostname" {
   description = "The hostname of the container"
 }
 
-variable "SIZE" {
-  type      = string
-  description = "The desired size for the LXC container's root filesystem (e.g., '8G', '20G')."
-  # Optional: Keep the validation
-  validation {
-    # Regex checks for one or more digits followed by G (for Gigabytes)
-    condition     = can(regex("^\\d+G$", var.SIZE))
-    error_message = "The rootfs size must be a number followed by 'G' (e.g., '8G')."
-  }
-}
-
 variable "pm_api_url" {
   type      = string
 }
