d9f339c8a6451c1c1cb5d0c23ffcc9f3b2ee2463
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-04-08 09:26:26 -0700

    Update main.tf

--- lxc_clone/main.tf
+++ lxc_clone/main.tf
@@ -2,6 +2,6 @@ resource "proxmox_lxc" "basic" {
   target_node = "pve04"
   hostname    = var.hostname
   #id of lxc container to clone
-  clone       = "lxc/126"
+  clone       = "126"
   # arch        = "arm64"
 }
